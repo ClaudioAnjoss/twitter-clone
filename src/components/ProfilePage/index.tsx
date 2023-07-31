a20a1074e4b3b09ff5ac31f3be2ddf7070778fd9
Claudio Anjos <claudio.anjoss@outlook.com>
2023-07-31 06:54:26 -0300

    Initial commit

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import {
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  Followage,
+  EditButton
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar></Avatar>
+      </Banner>
+      <ProfileData>
+        <EditButton outlined>Editar Perfil</EditButton>
+        <h1>Claudio Anjos</h1>
+        <h2>@claudio.anjos</h2>
+        <p>
+          Developer at <a href="#">Rocketseat</a>
+        </p>
+        <ul>
+          <li>
+            <LocationIcon />
+            São Paulo, Brasil
+          </li>
+          <li>
+            <CakeIcon />
+            Nascido(a) em 14 de agosto de 1997
+          </li>
+        </ul>
+
+        <Followage>
+          <span>
+            Seguindo <strong>94</strong>
+          </span>
+          <span>
+            <strong>607 </strong> Seguidores
+          </span>
+        </Followage>
+      </ProfileData>
+    </Container>
+  );
+};
+
+export default ProfilePage;
